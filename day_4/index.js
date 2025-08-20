1fba287fd7b5e31efa3d4e9844b8732ff370ccd0
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2025-08-20 10:33:00 +0530

    variables

--- day_4/index.js
+++ day_4/index.js
@@ -4,10 +4,29 @@
 // var a = 5;
 // console.log(a);
 
-const a = 20;
-console.log(a);
+// const a = 20;
+// console.log(a);
 // a = 45; not allowed
 // console.log(a);
 
-let b = 20;
+// let b = 20;
+// console.log(b);
+
+
+// scoping
+
+var a = 2;
+console.log(a);
+{
+    var a = 20;
+    console.log(a);
+}
+console.log(a);
+//let
+let b = 2;
+console.log(b);
+{
+    let b = 20;
+    console.log(b);
+}
 console.log(b);
